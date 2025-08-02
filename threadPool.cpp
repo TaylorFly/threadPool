8019f63f2ae659520001edcfdc30bda4341a3f3e
TaylorFly <taylorfly2003@gmail.com>
2025-08-02 18:01:49 +0800

    线程池

---
+++ threadPool.cpp
@@ -0,0 +1,85 @@
+#include "threadPool.hpp"
+#include <chrono>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <thread>
+
+constexpr int MAX_QUE_THRESHHOLD = 4;
+std::mutex outMux;
+
+ThreadPool::ThreadPool()
+    : initThreadSize_(4), taskCnt_(0),
+      taskQueSizeMaxThreshHold_(MAX_QUE_THRESHHOLD),
+      mode_(PoolMode::MODE_FIXED) {}
+
+void ThreadPool::setMode(PoolMode mode) { mode_ = mode; }
+void ThreadPool::taskQueSizeMaxThreshHold(size_t threshold) {
+  taskQueSizeMaxThreshHold_ = threshold;
+}
+
+void ThreadPool::submitTask(std::shared_ptr<Task> sp) {
+  std::unique_lock lock(taskQueMtx_);
+  if (!cv_nofull_.wait_for(lock, std::chrono::seconds(1), [&]() {
+    return taskQue_.size() < taskQueSizeMaxThreshHold_;
+  })) {
+    // taskQue_ is full after 1 seconds, so wait_for return false
+    std::cerr << "taskQue is full, submit task fail\n";
+  }
+  std::cout << "submit success\n";
+  taskQue_.emplace(sp);
+  taskCnt_++;
+  cv_noempty_.notify_all();
+}
+
+void ThreadPool::start(int initThreadSize) {
+  // 记录初始线程个数
+  initThreadSize_ = initThreadSize;
+  // 创建线程对象
+  for (int i = 0; i < initThreadSize_; i++) {
+    auto ptr =
+        std::make_unique<Thread>(std::bind(&ThreadPool::threadFunc, this));
+    this->threads_.emplace_back(std::move(ptr));
+  }
+  for (int i = 0; i < initThreadSize_; i++) {
+    threads_[i]->start();
+  }
+}
+
+// 启动线程
+void Thread::start() {
+  std::thread t(func_);
+  t.detach();
+}
+
+/**
+ * threads_中的每个thread会执行这个
+ */
+void ThreadPool::threadFunc() {
+  while (true) {
+    std::shared_ptr<Task> task;
+    {
+      std::unique_lock lock(taskQueMtx_);
+      std::cout << "tid: " << std::this_thread::get_id() << " try to get task: \n";
+      cv_noempty_.wait(lock, [&]() { return !taskQue_.empty(); });
+      task = taskQue_.front();
+      std::cout << "tid: " << std::this_thread::get_id() << " success to get task: \n";
+      taskQue_.pop();
+      taskCnt_--;
+
+      if (taskQue_.size() > 0) {
+        cv_noempty_.notify_all();
+      }
+
+      cv_nofull_.notify_all();
+    }
+    if (task != nullptr) {
+      task->run();
+    }
+  }
+}
+
+ThreadPool::~ThreadPool() {}
+Thread::Thread(ThreadFunc func) : func_(func) {}
+Thread::~Thread() {}
---
+++ threadPool.hpp
@@ -0,0 +1,66 @@
+#ifndef THREADPOLL_H
+#define THREADPOLL_H
+
+#include <atomic>
+#include <condition_variable>
+#include <cstddef>
+#include <functional>
+#include <memory>
+#include <mutex>
+#include <queue>
+#include <vector>
+
+// abstract class for task
+class Task {
+public:
+  //virtual 不能与模板一起使用
+  virtual void run() = 0;
+};
+
+enum class PoolMode { MODE_FIXED, MODE_CACHED };
+
+class Thread {
+  using ThreadFunc = std::function<void()>;
+
+public:
+  Thread(ThreadFunc);
+  ~Thread();
+  // 启动线程
+  void start();
+
+private:
+  ThreadFunc func_;
+};
+
+class ThreadPool {
+public:
+  ThreadPool();
+  ~ThreadPool();
+
+  void setMode(PoolMode mode);
+  void taskQueSizeMaxThreshHold(size_t threshold);
+  void submitTask(std::shared_ptr<Task> sp);
+  void start(int initThreadSize = 4);
+
+  ThreadPool(const ThreadPool &) = delete;
+  ThreadPool &operator=(const ThreadPool &) = delete;
+
+private:
+  void threadFunc();
+
+private:
+  std::vector<std::unique_ptr<Thread>> threads_;
+  size_t initThreadSize_; // initial thread number
+
+  std::queue<std::shared_ptr<Task>> taskQue_;
+  std::atomic_uint taskCnt_;
+  int taskQueSizeMaxThreshHold_;
+
+  std::mutex taskQueMtx_;
+  // 两个的好处是可以避免伪唤醒
+  std::condition_variable cv_noempty_, cv_nofull_;
+
+  PoolMode mode_;
+};
+
+#endif
